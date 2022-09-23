433312bd2bc4c2c3d351be1aa31c5b87dfc50731
raohassan4212 <52700173+raohassan4212@users.noreply.github.com>
2022-09-23 23:48:57 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import "./App.css";
+import QRCode from "qrcode";
+import { useState } from "react";
+
+function App() {
+  const [url, setUrl] = useState("");
+  const [qrcode, setQrcode] = useState("");
+
+  const GenerateQRCode = () => {
+    QRCode.toDataURL(url, (err, url) => {
+      if (err) return console.log("error 01", err);
+      console.log("success", url);
+      setQrcode(url);
+    });
+  };
+  return (
+    <div className="App">
+      <h1>QR Code Generator</h1>
+      <input
+        type="text"
+        placeholder="e.g. https://google.com"
+        value={url}
+        onChange={(e) => setUrl(e.target.value)}
+      />
+      <button onClick={GenerateQRCode}>Generate</button>
+      {qrcode && (
+        <>
+          <img src={qrcode} />
+          <a href={qrcode} download="qrcode.png">
+            Download
+          </a>
+        </>
+      )}
+    </div>
+  );
+}
+
+export default App;
